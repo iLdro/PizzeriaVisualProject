0be0b5fff419565990fd8153a074fdd0472d6094
Edran0111 <82849327+Edran0111@users.noreply.github.com>
2023-10-20 17:37:40 +0200

    Adding delivery system

---
+++ PizzeriaVisual/Services/DeliveryServices.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using PizzeriaVisual.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzeriaVisual.Services
+{
+    internal class DeliveryServices : IDeliveryServices
+    {
+        public List<Delivery> delivers;
+
+        public DeliveryServices()
+        {
+            delivers = new List<Delivery>();
+            delivers = DatabaseManager.AllItems<Delivery>("C:\\Users\\adria\\source\\repos\\PizzeriaVisualProject\\PizzeriaVisual\\Databases\\Delivery.json");
+            Console.WriteLine(delivers);
+        }
+
+        public int CreateDelivery(string name, string surname, string address, string phoneNumber)
+        {
+            int lastDeliveryId = delivers.Count > 0 ? delivers.Max(c => c.Id) : 0;
+            int newDeliveryId = lastDeliveryId + 1;
+
+            Delivery delivery = new Delivery(newDeliveryId, name, surname, address, phoneNumber);
+
+            DatabaseManager.CreateItem(delivery, "C:\\Users\\adria\\source\\repos\\PizzeriaVisualProject\\PizzeriaVisual\\Databases\\Delivery.json");
+            return newDeliveryId;
+        }
+    }
+
+}
